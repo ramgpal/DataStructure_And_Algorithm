2509cc63c9c111abff171be4134410a6a49de071
ramgpal <ramgpal912@gmail.com>
2023-03-16 17:21:09 +0530

    Commit

---
+++ Array/PrintSubarrays.java
@@ -0,0 +1,22 @@
+package Array;
+
+public class PrintSubarrays {
+    public static void printSubarrays(int nums []) {
+        for(int i=0;i<nums.length;i++) {
+            for(int j=i;j<nums.length;j++) {
+                //loop to print
+            for(int k=i;k<=j;k++) {
+                System.out.print(nums[k]+" "); //subarrays
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+            }
+       public static void main (String[] args) {
+            int nums[] = {2,4,6,8,10};
+         printSubarrays(nums);
+        }
+    }
+
+    //Total subarrays = (n*(n+1))/2
