b66580dd967c45a63c7ab696d6313a8163575a23
ramgpal <ramgpal912@gmail.com>
2023-06-01 20:48:49 +0530

    firstCommit

---
+++ BinarayTree/preorderTraversal.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class preorderTraversal {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    
+    static class BinaryTee {
+        static int index = -1;
+        public static Node buildTree(int nodes[]) {
+            index++;
+            if(nodes[index] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[index]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+    public static void preorder(Node root) {
+        if(root == null) {
+            return;
+        }
+        System.out.print(root.data+" ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+}
+    public static void main(String[] args) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        BinaryTee tree = new BinaryTee();
+        Node root = tree.buildTree(nodes);
+        tree.preorder(root);
+
+    }
+}
