70e2331c872f879018eb7ea6cecd9758d0ed3832
ramgpal <ramgpal912@gmail.com>
2023-06-01 22:53:20 +0530

    2ndCommit

---
+++ BinarayTree/treeTraversal.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+public class treeTraversal {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTee {
+        static int index = -1;
+
+        public static Node buildTree(int nodes[]) {
+            index++;
+            if (nodes[index] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[index]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+
+        // Preorder Traversal - O(n) = rLR
+        public static void preorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+
+        // Inorder Traversal - O(n) = LrR
+        public static void inorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+
+        // Postorder Traversal - O(n) = LRr
+        public static void postorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTee tree = new BinaryTee();
+        Node root = tree.buildTree(nodes);
+        // tree.preorder(root);
+        // tree.inorder(root);
+        tree.postorder(root);
+
+    }
+}
