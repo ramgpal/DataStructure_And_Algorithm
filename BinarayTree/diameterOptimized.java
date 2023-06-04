b4a26dc40666e1a5391eef240267e077819d707c
ramgpal <ramgpal912@gmail.com>
2023-06-04 12:03:37 +0530

    6thCommit

--- BinarayTree/diameterOptimized.java
+++ BinarayTree/diameterOptimized.java
@@ -1,3 +1,47 @@
-public class diameterOptimized {
+import java.util.*;
 
+public class diameterOptimized {
+    public static class Node {
+        int data;
+        Node right;
+        Node left;
+
+        Node(int data) {
+            this.data = data;
+            this.right = null;
+            this.left = null;
+        }
+    }
+
+    static class Info {
+        int diam;
+        int ht;
+
+        public Info(int diam, int ht) {
+            this.diam = diam;
+            this.ht = ht;
+        }
+    }
+
+    public static Info diameter(Node root) {
+        if (root == null) {
+            return new Info(0, 0);
+        }
+        Info leftInfo = diameter(root.left);
+        Info rightInfo = diameter(root.right);
+        int diam = Math.max(Math.max(leftInfo.diam, rightInfo.diam), leftInfo.ht + rightInfo.ht + 1);
+        int ht = Math.max(leftInfo.ht, rightInfo.ht) + 1;
+        return new Info(diam, ht);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        System.out.println("Diameter of the tree is: " + diameter(root).diam);
+    }
 }
