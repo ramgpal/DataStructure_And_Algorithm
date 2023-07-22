d8161016551b41072c254b4f25dde48cc262d9c5
ramgpal <ramgpal912@gmail.com>
2023-07-22 18:50:53 +0530

    4thCommit

---
+++ Graph/hasPath.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class hasPath {
+    public class Edge{
+        int src;
+        int dest;
+        int weight;
+        public Edge(int s, int d, int w) {
+            this.src = s;
+            this.dest = d;
+            this.weight = w;
+        }
+    }
+}
