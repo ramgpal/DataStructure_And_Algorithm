a8a409ea13ef9a5f04d90700c8b0164139dfcd97
ramgpal <ramgpal912@gmail.com>
2023-08-02 00:06:52 +0530

    1stCommit

--- Recursion_And_BackTracking/Subsets.java
+++ Recursion_And_BackTracking/Subsets.java
@@ -1,26 +1,54 @@
+// TC - O(n*2^n)
 import java.util.*;
 public class Subsets {
-    public static void Find_Subsets(String str,String ans,int index) {
-        //Base
-if(index==str.length()) {
-    if(ans.length()==0){
-     System.out.println("NULL");   
+    public static void findSubsets(String str, String ans, int i) {
+    // Base case
+        if(i == str.length()) {
+            if(ans.length() == 0) {
+                System.out.println("null");
+            }
+            else {
+                System.out.println(ans);
+            }
+            return;
+        }
+    // Kaam
+        //yes choice
+        findSubsets(str, ans+str.charAt(i), i+1);
+        //No choice
+        findSubsets(str, ans, i+1);
     }
-    else {
-System.out.println(ans);
+    public static void main(String[] args) {
+        String str = "abc";
+        String temp = "";
+        int idx = 0;
+        findSubsets( str, temp, idx);
+    }
 }
-return;
+
+
+// 78. Leetcode solution
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+        findSubsets(nums, ans, curr, 0);
+        return ans;
+    }
+    public void findSubsets(int[] nums, List<List<Integer>> ans, List<Integer> curr, int idx) {
+        // Base case: If we have processed all elements, add the current subset to the answer
+        if (idx == nums.length) {
+            ans.add(new ArrayList<>(curr));
+            return;
+        }
+
+        // Recursive calls: Explore two possibilities - including or excluding the current element
+        // Choice 1: Include the current element
+        curr.add(nums[idx]);
+        findSubsets(nums, ans, curr, idx + 1);
+
+        // Choice 2: Exclude the current element
+        curr.remove(curr.size() - 1);
+        findSubsets(nums, ans, curr, idx + 1);
+    }
 }
-//Recursive aproach
-//choice Yes
-Find_Subsets(str, ans+str.charAt(index), index+1);
-//Choice No
-Find_Subsets(str, ans, index+1); 
-}
-public static void main (String[] args) {
-    String str="abc";
-    String ans="";
-    Find_Subsets(str, ans, 0);
-}
-}
-//Time Complexity O(n*2^n)
