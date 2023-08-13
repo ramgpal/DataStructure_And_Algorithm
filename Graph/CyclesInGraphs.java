0eac22d4c9ee984bf2bb6faa6eecd3daeed61ce5
ramgpal <ramgpal912@gmail.com>
2023-08-13 17:08:24 +0530

    2ndCommit

--- Graph/CyclesInGraphs.java
+++ Graph/CyclesInGraphs.java
@@ -14,13 +14,13 @@ public class CyclesInGraphs {
     }
     // vertex -> 0
     graph[0].add(new Edge(0, 1));
-    // graph[0].add(new Edge(0, 2));
+    graph[0].add(new Edge(0, 2));
     graph[0].add(new Edge(0, 3));
 
     graph[1].add(new Edge(1, 0));
     graph[1].add(new Edge(1, 2));
 
-    // graph[2].add(new Edge(2, 0));
+    graph[2].add(new Edge(2, 0));
     graph[2].add(new Edge(2, 1));
 
     graph[3].add(new Edge(3, 0));
