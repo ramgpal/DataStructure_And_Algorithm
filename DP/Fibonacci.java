df0fd88db0b982aa80aeba80c641867c127e70fa
ramgpal <ramgpal912@gmail.com>
2023-09-11 11:04:30 +0530

    3rdCommit

--- DP/Fibonacci.java
+++ DP/Fibonacci.java
@@ -1,18 +1,34 @@
 import java.util.*;
 public class Fibonacci {
-    public static int fib(int n, int [] f) { // TC -> O(n)
-        if(n == 0 || n == 1) {
-            return n;
+
+// Memoization
+    // public static int fib(int n, int [] f) { // TC -> O(n)
+    //     if(n == 0 || n == 1) {
+    //         return n;
+    //     }
+    //     if(f[n] != 0) { // if fib(n) is already calculated
+    //         return f[n];
+    //     }
+    //     f[n] = fib(n-1, f)+fib(n-2, f);
+    //     return f[n];
+    // }
+    
+// Tabulation
+    public static int fibTabulation(int n) {
+        int dp[] = new int[n+1];
+        // initialisation
+        dp[0] = 0;
+        dp[1] = 1;
+        // filling - Bottom Up
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1]+ dp[i-2];
         }
-        if(f[n] != 0) { // if fib(n) is already calculated
-            return f[n];
-        }
-        f[n] = fib(n-1, f)+fib(n-2, f);
-        return f[n];
+        return dp[n];
     }
     public static void main(String[] args) {
         int n = 6;
-        int f[] = new int[n+1]; // initially  all values in the array will be zero
-        System.out.println(fib(n, f));
+        // int f[] = new int[n+1]; // initially  all values in the array will be zero
+        // System.out.println(fib(n, f));
+        System.out.println(fibTabulation(n));
     }
 }
