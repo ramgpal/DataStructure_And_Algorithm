1cd0230637ce9157695e57da446ab8893caf5b72
ramgpal <ramgpal912@gmail.com>
2023-09-12 10:28:33 +0530

    4thCommit

--- DP/climbingStairs.java
+++ DP/climbingStairs.java
@@ -28,19 +28,24 @@ public class climbingStairs {
     }
 
 // Tabulation O(n)
-     public static int countWays(int n) {
-        if(n < 0) {
-            return 0;
+     public static int countWaysTabulation(int n) {
+       int dp[] = new int[n+1];
+       dp[0] = 1;
+       for(int i = 1; i <= n; i++) {
+        if(i == 1) {
+            dp[i] = dp[i-1]+0;
         }
-        if(n==0) {
-            return 1;
+        else {
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        return countWays(n-1)+countWays(n-2);
+       }
+       return dp[n];
     }
     public static void main(String[] args) {
         int n = 5;
-        int ways[] = new int[n+1];
         System.out.println(countWays(n));  // 1st function call
+        int ways[] = new int[n+1];
         System.out.println(countWaysMemoization(n, ways));  // 2nd fuction call
+        System.out.println(countWaysTabulation(n)); // 3rd function call
     }
 }
