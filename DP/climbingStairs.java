0ab66884c19ecec41570af467824e3815f315137
ramgpal <ramgpal912@gmail.com>
2023-09-12 09:47:40 +0530

    3rdCommit

--- DP/climbingStairs.java
+++ DP/climbingStairs.java
@@ -1,7 +1,34 @@
 import java.util.*;
 public class climbingStairs {
-// Basic Recursion
+
+// Basic Recursion O(2^n)
     public static int countWays(int n) {
+        if(n < 0) {
+            return 0;
+        }
+        if(n==0) {
+            return 1;
+        }
+        return countWays(n-1)+countWays(n-2);
+    }
+
+// Memoization O(n)
+    public static int countWaysMemoization(int n, int ways[]) {
+        if(n < 0) {
+            return 0;
+        }
+        if(n==0) {
+            return 1;
+        }
+        if(ways[n] != 0) {
+            return ways[n];
+        }
+        ways[n] = countWaysMemoization(n-1, ways)+countWaysMemoization(n-2, ways);
+        return ways[n];
+    }
+
+// Tabulation O(n)
+     public static int countWays(int n) {
         if(n < 0) {
             return 0;
         }
@@ -12,6 +39,8 @@ public class climbingStairs {
     }
     public static void main(String[] args) {
         int n = 5;
-        System.out.println(countWays(n));
+        int ways[] = new int[n+1];
+        System.out.println(countWays(n));  // 1st function call
+        System.out.println(countWaysMemoization(n, ways));  // 2nd fuction call
     }
 }
