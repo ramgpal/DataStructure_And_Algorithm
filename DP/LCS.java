7369580ba7c001e8242a3a02c1eaad7cb40187bd
ramgpal <ramgpal912@gmail.com>
2023-09-30 12:23:12 +0530

    3rd Commit

--- DP/LCS.java
+++ DP/LCS.java
@@ -16,24 +16,51 @@ public class LCS {
             return Math.max(ans1, ans2);
         }
     }
-// Memoization
+// Memoization -> O(n*m)
     public static int lcsMemoization(String str1, String str2, int n, int m, int dp[][]) {
-        if(n == 0 || m == 0) {
+        if(n == 0 || m == 0){
             return 0;
         }
-        if(dp[n][m] != 0) {
+
+        if(dp[n][m] != -1){
             return dp[n][m];
         }
-        if(str1.charAt(n-1) == str2.charAt(m-1)) {
-            dp[n][m] = lcsMemoization(str1, str2, n-1, m-1, dp);
+
+        if(str1.charAt(n-1) == str2.charAt(m-1)){
+            dp[n][m] = lcsMemoization(str1, str2, n-1, m-1, dp)+1;
             return dp[n][m];
         }
-        else {
+        else{
             int ans1 = lcsMemoization(str1, str2, n-1, m, dp);
-            int ans2 = lcsMemoization(str1, str2, ans1, m-1, dp);
+            int ans2 = lcsMemoization(str1, str2, n, m-1, dp);
             dp[n][m] = Math.max(ans1, ans2);
             return dp[n][m];
         }
+    }
+// Tabulation ->
+    public static int lcsTabulation(String str1, String str2, int n, int m) {
+        int dp[][] = new int[n+1][m+1]; // dp table
+        //Initialisating dp table
+        for(int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+        for(int j = 0; j <= m; j++) {
+            dp[0][j] = 0;
+        }
+        // Filling DP table
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(str1.charAt(i-1) == str2.charAt(j-1)) { // same
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }
+                else {
+                    int ans1 = dp[i-1][j];
+                    int ans2 = dp[i][j-1];
+                    dp[i][j] = Math.max(ans1, ans2);
+                }
+            }
+        }
+        return dp[n][m];
     }
     public static void main(String[] args) {
         String str1 = "abcdge";
@@ -41,7 +68,15 @@ public class LCS {
         int n = str1.length();
         int m = str2.length();
         System.out.println(lcsRecursion(str1, str2, n, m));
+        
         int dp[][] = new int[n+1][m+1];
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j <= m; j++) {
+                dp[i][j] = -1;
+            }
+        }
         System.out.println(lcsMemoization(str1, str2, n, m, dp));
+
+        System.out.println(lcsTabulation(str1, str2, n, m));
     }
 }
